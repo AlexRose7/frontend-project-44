146123d73a569e736e50b20c2e6dbf22d1df578f
Alex Rose <sandro@MacBook-Air-Alex.local>
2023-03-16 16:05:40 +0300

    modified:   src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 function greetings() {
-  const userName = readlineSync.question("May I have your name?");
-  return "Hello, " + userName + "!";
+  const userName = readlineSync.question('May I have your name?');
+  return `Hello, ${userName}!`;
 }
 
 export default greetings;
