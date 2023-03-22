1d6ebede7e57f82b506e30128fa1ee7842d7cd29
Alex Rose <sandro@MacBook-Air-Alex.local>
2023-03-23 01:19:16 +0300

    add calc.js even.js

---
+++ src/games/calc.js
@@ -0,0 +1,35 @@
+import getRandomNum from "../random.js";
+import gameStart from "../index.js";
+
+const taskDeskription = "What is the result of the expression?";
+
+function calculated(num1, num2, method) {
+  let res;
+  switch (method) {
+    case "+":
+      res = num1 + num2;
+      break;
+    case "-":
+      res = num1 - num2;
+      break;
+    case "*":
+      res = num1 * num2;
+      break;
+    default:
+  }
+  return res;
+}
+
+function getGameData() {
+  const randomNum1 = getRandomNum(1, 100);
+  const randomNum2 = getRandomNum(1, 100);
+  const sings = ["+", "-", "*"];
+  const sing = sings[getRandomNum(0, sings.length - 1)];
+  const question = `${randomNum1} ${sing} ${randomNum2}`;
+  const correctAnswer = calculated(randomNum1, randomNum2, sing).toString();
+  return [question, correctAnswer];
+}
+
+const getGame = () => gameStart(taskDeskription, getGameData);
+
+export default getGame;
---
+++ src/games/even.js
@@ -0,0 +1,17 @@
+import getRandomNum from "../random.js";
+import gameStart from "../index.js";
+
+const taskDeskription =
+  'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (num) => num % 2 === 0;
+
+function getGameData() {
+  const randomNum = getRandomNum(1, 100);
+  const correctAnswer = isEven(randomNum) ? "yes" : "no";
+  return [randomNum, correctAnswer];
+}
+
+const getGame = () => gameStart(taskDeskription, getGameData);
+
+export default getGame;
