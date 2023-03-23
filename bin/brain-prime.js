fa1199a98031aca0d09226e3e0208d1c4082185e
Alex Rose <sandro@MacBook-Air-Alex.local>
2023-03-23 21:40:42 +0300

    add new game data

---
+++ bin/brain-prime.js
@@ -0,0 +1,3 @@
+import getGame from "../src/games/prime.js";
+
+getGame();
---
+++ src/games/prime.js
@@ -0,0 +1,23 @@
+import getRandomNum from "../random.js";
+import gameStart from "../index.js";
+
+const taskDeskription =
+  'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+function isPrime(num) {
+  if (num % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function getGameData() {
+  const randomNum = getRandomNum(1, 9);
+  const correctAnswer = isPrime(randomNum) ? "yes" : "no";
+  return [randomNum, correctAnswer];
+}
+
+const getGame = () => gameStart(taskDeskription, getGameData);
+
+export default getGame;
