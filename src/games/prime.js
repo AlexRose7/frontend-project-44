9b202cfc6e15690366a81c3bdba8152489564e98
Alex Rose <sandro@MacBook-Air-Alex.local>
2023-03-24 18:33:36 +0300

    fixed

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,19 +1,20 @@
-import getRandomNum from "../random.js";
-import gameStart from "../index.js";
+import getRandomNum from '../random.js';
+import gameStart from '../index.js';
 
-const taskDeskription =
-  'Answer "yes" if given number is prime. Otherwise answer "no".';
+const taskDeskription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 function isPrime(num) {
-  if (num % 2 === 0) {
-    return false;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
   }
   return num > 1;
 }
 
 function getGameData() {
   const randomNum = getRandomNum(1, 9);
-  const correctAnswer = isPrime(randomNum) ? "yes" : "no";
+  const correctAnswer = isPrime(randomNum) ? 'yes' : 'no';
   return [randomNum, correctAnswer];
 }
 
